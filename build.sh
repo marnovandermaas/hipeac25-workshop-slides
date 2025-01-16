313de177dfb5789dfad4856bdc8ffa300ddaecb0
Marno van der Maas <mvdmaas+git@lowrisc.org>
2025-01-16 14:52:12 +0000

    Handout re-enabled

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 rm *.aux *.log *.nav *.out *.snm *.toc handout.pdf slides.pdf notes.pdf
-#pdflatex handout
+pdflatex handout
 pdflatex slides
 #pdflatex notes
 echo "----- Rerunning for bookmarks -----"
-#pdflatex handout
+pdflatex handout
 pdflatex slides
 #pdflatex notes
